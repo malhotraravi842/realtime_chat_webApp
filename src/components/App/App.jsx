5a9d22ff8acdc5c6b8245f84bb69f9d6ffe07f13
Ravi Chaudhary <malhotraravi842@gmail.com>
2021-08-10 20:17:12 +0530

    createUser script setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,12 @@
-import { useEffect } from 'react';
-import { fb } from 'service';
+import { Route, Switch } from 'react-router';
+import { Login, Signup, Chat } from 'components';
 
 export const App = () => {
-  useEffect(() => {
-    fb.firestore
-      .collection('chatUsers')
-      .where('userName', '==', 'rc069056')
-      .get()
-      .then(res => {
-        const user = res.docs[0]?.data();
-        console.log(user);
-      });
-  }, []);
-
-  return <div>Hello Ravi</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Chat} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
+  );
 };
