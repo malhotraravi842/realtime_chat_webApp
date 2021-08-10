7d9f589541f948654bb1ddaaef6b97268d587e2a
Ravi Chaudhary <malhotraravi842@gmail.com>
2021-08-10 13:46:35 +0530

    configured firebase and chatengine.io

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,17 @@
-export const App = () => <>Hello from PortEXE</>;
+import { useEffect } from 'react';
+import { fb } from 'service';
+
+export const App = () => {
+  useEffect(() => {
+    fb.firestore
+      .collection('chatUsers')
+      .where('userName', '==', 'rc069056')
+      .get()
+      .then(res => {
+        const user = res.docs[0]?.data();
+        console.log(user);
+      });
+  }, []);
+
+  return <div>Hello Ravi</div>;
+};
